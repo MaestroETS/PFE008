67b8ea2a4e19152adf0db67fd2cfac1db05a24b4
jeremy <jere.dacosta@outlook.com>
2024-06-16 15:47:16 -0400

    allow all cors

--- backend/src/main/java/PFE008/backend/ConvertController.java
+++ backend/src/main/java/PFE008/backend/ConvertController.java
@@ -11,11 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -35,6 +31,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class ConvertController {
 
 	@PostMapping("/convert")
+    @CrossOrigin(origins = "*")
 	public ResponseEntity<?> convert(@RequestParam("file") MultipartFile multipartFile) throws IOException {
 		FileUtil downloadUtil = new FileUtil();
 		
